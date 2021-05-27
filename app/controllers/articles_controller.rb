2aa6b2be1eedd45eac017018341d3e5ba0b6f174
Cendevs <cndigitech@gmail.com>
2021-05-27 12:33:45 +0100

    add more features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,13 +12,39 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
       render :new
     end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
+  def back
+    redirect_to root_path
+  end
+
 
   private
     def article_params
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,14 @@
 class Article < ApplicationRecord
+    has_many :comments
+    
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
+
+    VALID_STATUS = ['public', 'private', 'archived']
+
+    validates :status, inclusion: { in: VALID_STATUS }
+
+    def archived?
+        status == 'achived'
+    end
 end
